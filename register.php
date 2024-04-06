a194d2f480bf5139d42dba48284ef324f82a6096
Kevin Ramses <kevinramsesgamer@gmail.com>
2024-04-06 01:46:46 -0600

    Yo creo que quedo bien

--- register.php
+++ register.php
@@ -1,24 +1,21 @@
 <?php
-// Conexión a la base de datos
-$servername = "localhost"; // Cambia esto por tu servidor MySQL
-$username = "id22014009_dbparatodo_1"; // Cambia esto por tu nombre de usuario de MySQL
-$password = "Fate2020."; // Cambia esto por tu contraseña de MySQL
-$dbname = "id22014009_dbparatodo_1"; // Cambia esto por el nombre de tu base de datos
+$servername = "localhost"; 
+$username = "id22014009_dbparatodo_1"; 
+$password = "Fate2020."; 
+$dbname = "id22014009_dbparatodo_1"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Procesar el formulario de registro
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['logname'];
     $email = $_POST['logemail'];
     $password = $_POST['logpass'];
 
-    // Consulta para insertar el nuevo usuario en la base de datos
     $sql = "INSERT INTO usuarios (username, email, password) VALUES ('$username', '$email', '$password')";
 
     if ($conn->query($sql) === TRUE) {
@@ -28,6 +25,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// Cerrar la conexión a la base de datos
 $conn->close();
 ?>
